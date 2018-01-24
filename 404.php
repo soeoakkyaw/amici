f9634c92fee21308777992277a24c50923358bf5
Soe Oak Kyaw <soe@Soes-MacBook-Pro.local>
2018-01-24 10:29:34 +0800

    404 update

--- 404.php
+++ 404.php
@@ -22,7 +22,7 @@ get_header(); ?>
 
 	<div id="primary" class="content-area container">
 
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main main" role="main">
 
 
 
